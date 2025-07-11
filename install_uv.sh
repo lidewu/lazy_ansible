28b251d0f0ec49930557031214bc742eadfa4253
lide <lide@lidedeMacBook-Pro.local>
2025-07-11 11:52:34 +0800

    1. use uv to management project. \n 2. remove requirements.txt. 3. add MIT LICENSE.

--- install_uv.sh
+++ install_uv.sh
@@ -8,21 +8,5 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 . $HOME/.local/bin/env
 
-echo "Install Python3.12.11"
-uv python install 3.12.11
-
-
-echo "Create VENV For Python3.12.11"
-uv venv -p 3.12.11
-
-echo "Install pip in .venv" 
-curl -sS https://bootstrap.pypa.io/get-pip.py | /home/sky/self_ansible/.venv/bin/python3
-
-echo "Enter to .venv"
-. .venv/bin/activate
-
-echo "Install Asnible"
-pip3 install -r ./reqirements.txt
-
-echo "Leave .venv"
-deactivate
+echo "run uv sync"
+uv sync
